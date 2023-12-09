d80bf1cb14186d09dd49d673039c6083e6d80697
tyVespA <migliavacca.marco08@gmail.com>
2023-12-09 10:48:05 +0100

    Install ejs, create views using ejs, add style

--- 07_ejs_demo.js
+++ 07_ejs_demo.js
@@ -10,20 +10,27 @@ app.set("views", "07_views");
 
 app.listen(3000);
 
+const blogs = [
+  { title: "Blog title 1", snippet: "Lorem ipsum snippet" },
+  { title: "Blog title 2", snippet: "Lorem ipsum snippet" },
+  { title: "Blog title 3", snippet: "Lorem ipsum snippet" },
+];
+
 app.get("/", (req, res) => {
   // res.sendFile("./05_views/index.html", { root: __dirname });
   // we no longer send a file but we render a view with .render
-  res.render("index");
+  // i can pass data from this handler into the view by passing an object as the 2nd param
+  res.render("index", { title: "Home", blogs: blogs });
 });
 
 app.get("/about", (req, res) => {
-  res.render("about");
+  res.render("about", { title: "About" });
 });
 
 app.get("/blogs/create", (req, res) => {
-  res.render("create");
+  res.render("create", { title: "Create" });
 });
 
 app.use((req, res) => {
-  res.status(404).render("404");
+  res.status(404).render("404", { title: "404" });
 });
